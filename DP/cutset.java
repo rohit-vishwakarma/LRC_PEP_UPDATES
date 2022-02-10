2f91dbcee13b437d263a3ee8c9252bb48e26c0fb
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-02-10 14:38:07 +0530

    commit

--- DP/cutset.java
+++ DP/cutset.java
@@ -1,27 +1,106 @@
 import java.util.*;
+
 public class cutset {
-    //Matrix chain multiplication gfg
-    static int mcm_memo(int[] arr, int si, int ei, int[][] dp){
-        if(si + 1 == ei){
+    // Matrix chain multiplication gfg
+    static int mcm_memo(int[] arr, int si, int ei, int[][] dp) {
+        if (si + 1 == ei) {
             return dp[si][ei] = 0;
         }
-        if(dp[si][ei] !=  -1) return dp[si][ei];
+        if (dp[si][ei] != -1)
+            return dp[si][ei];
         int min = Integer.MAX_VALUE;
-        for(int cut = si + 1; cut<ei; cut++){
+        for (int cut = si + 1; cut < ei; cut++) {
             int leftres = mcm_memo(arr, si, cut, dp);
             int rightres = mcm_memo(arr, cut, ei, dp);
 
-            int res = leftres + arr[si]*arr[cut]*arr[ei] + rightres;
+            int res = leftres + arr[si] * arr[cut] * arr[ei] + rightres;
             min = Math.min(res, min);
         }
         return dp[si][ei] = min;
     }
-    static int matrixMultiplication(int N, int arr[])
-    {
+
+    static int matrixMultiplication(int N, int arr[]) {
         // code here
         int[][] dp = new int[N][N];
-        for(int[] a: dp) Arrays.fill(a, -1);
-        return mcm_memo(arr,0,N-1, dp);
+        for (int[] a : dp)
+            Arrays.fill(a, -1);
+        return mcm_memo(arr, 0, N - 1, dp);
     }
 
+    // MinMaxEvaluation "1+2*3+4+5*6" gfg
+    public static class minMaxPair {
+        int minvalue = (int) 1e9;
+        int maxvalue = -(int) 1e9;
+
+        minMaxPair() {
+
+        }
+
+        minMaxPair(int mn, int mx) {
+            minvalue = mn;
+            maxvalue = mx;
+        }
+    }
+
+    public static minMaxPair Evaluation(minMaxPair lp, minMaxPair rp, char op) {
+        if (op == '+') {
+            return new minMaxPair(lp.minvalue + rp.minvalue, lp.maxvalue + rp.maxvalue);
+        } else {
+            return new minMaxPair(lp.minvalue * rp.minvalue, lp.maxvalue * rp.maxvalue);
+        }
+    }
+
+    public static minMaxPair minMaxEvaluation(String s, int si, int ei, minMaxPair[][] dp) {
+        if (si == ei) {
+            int val = s.charAt(si) - '0';
+            return new minMaxPair(val, val);
+        }
+        if (dp[si][ei] != null)
+            return dp[si][ei];
+
+        minMaxPair myres = new minMaxPair();
+        for (int cut = si + 1; cut < ei; cut += 2) {
+            minMaxPair lr = minMaxEvaluation(s, si, cut - 1, dp);
+            minMaxPair rr = minMaxEvaluation(s, cut + 1, ei, dp);
+
+            minMaxPair ev = Evaluation(lr, rr, s.charAt(cut));
+            myres.minvalue = Math.min(myres.minvalue, ev.minvalue);
+            myres.maxvalue = Math.max(myres.maxvalue, ev.maxvalue);
+        }
+        return dp[si][ei] = myres;
+    }
+
+    // 312 burst balloons
+    public int burst(int[] arr, int si, int ei, int[][] dp) {
+        if (dp[si][ei] != 0)
+            return dp[si][ei];
+
+        int lval = si == 0 ? 1 : arr[si - 1];
+        int rval = ei == arr.length - 1 ? 1 : arr[ei + 1];
+
+        int maxcost = 0;
+        for (int cut = si; cut <= ei; cut++) {
+            int lc = cut == si ? 0 : burst(arr, si, cut - 1, dp);
+            int rc = cut == ei ? 0 : burst(arr, cut + 1, ei, dp);
+
+            int val = lc + lval * arr[cut] * rval + rc;
+            maxcost = Math.max(maxcost, val);
+        }
+        return dp[si][ei] = maxcost;
+    }
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        return burst(nums, 0, n - 1, dp);
+    }
+
+    public static void main(String[] args) {
+        String s = "1+2*3+4*5";
+        int n = s.length();
+        minMaxPair[][] dp = new minMaxPair[n][n];
+        minMaxPair res = minMaxEvaluation(s, 0, n - 1, dp);
+        System.out.println(res.minvalue);
+        System.out.println(res.maxvalue);
+    }
 }
--- DP/targetset.java
+++ DP/targetset.java
@@ -1,198 +1,258 @@
 import java.util.*;
+
 public class targetset {
-    
-    public static int permutation_memo(int[] arr, int tar, int[] dp){
-        if(tar == 0) return dp[tar] = 1;
-        if(dp[tar] != -1) return dp[tar];
+
+    public static int permutation_memo(int[] arr, int tar, int[] dp) {
+        if (tar == 0)
+            return dp[tar] = 1;
+        if (dp[tar] != -1)
+            return dp[tar];
         int count = 0;
-        for(int ele : arr){
-            if(tar - ele >= 0){
+        for (int ele : arr) {
+            if (tar - ele >= 0) {
                 count += permutation_memo(arr, tar - ele, dp);
             }
         }
         return dp[tar] = count;
     }
 
-    public static int combination_memo(int[] arr, int tar, int idx, int[][] dp){
-        if(tar == 0) return dp[idx][tar] = 1;
-        if(dp[idx][tar] != -1) return dp[idx][tar];
+    public static int combination_memo(int[] arr, int tar, int idx, int[][] dp) {
+        if (tar == 0)
+            return dp[idx][tar] = 1;
+        if (dp[idx][tar] != -1)
+            return dp[idx][tar];
         int count = 0;
-        for(int i=idx; i<arr.length; i++){
-            if(tar - arr[i] >=0){
+        for (int i = idx; i < arr.length; i++) {
+            if (tar - arr[i] >= 0) {
                 count += combination_memo(arr, tar - arr[i], i, dp);
             }
         }
         return dp[idx][tar] = count;
     }
 
-    public static void display(int[] arr){
-        for(int ele: arr){
-            System.out.print(ele+" ");
+    public static void display(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
         System.out.println();
     }
 
-    public static void display2D(int[][] arr){
-        for(int[] a: arr){
+    public static void display2D(int[][] arr) {
+        for (int[] a : arr) {
             display(a);
         }
     }
-    //322 Coin Change
+
+    // 322 Coin Change
     public int coinChange(int[] coins, int amount) {
         // int n = coins.length;
-        int[] dp = new int[amount+1];
-       
-        Arrays.fill(dp, (int)1e9);
-       
+        int[] dp = new int[amount + 1];
+
+        Arrays.fill(dp, (int) 1e9);
+
         int ans = coinchange(coins, amount, dp);
-        return ans >= (int)1e8 ? -1 : ans;
+        return ans >= (int) 1e8 ? -1 : ans;
     }
-    public int coinchange(int[] arr,  int tar, int[] dp){
-        if(tar == 0 ) return dp[tar] = 0;
-        
-        if(dp[tar] != (int)1e9) return dp[tar];
-        int min = (int)1e8;
-        for(int i=0; i<arr.length; i++){
-            if(tar - arr[i] >= 0){
-                min = Math.min(coinchange(arr, tar - arr[i], dp)+1, min);
+
+    public int coinchange(int[] arr, int tar, int[] dp) {
+        if (tar == 0)
+            return dp[tar] = 0;
+
+        if (dp[tar] != (int) 1e9)
+            return dp[tar];
+        int min = (int) 1e8;
+        for (int i = 0; i < arr.length; i++) {
+            if (tar - arr[i] >= 0) {
+                min = Math.min(coinchange(arr, tar - arr[i], dp) + 1, min);
             }
         }
         return dp[tar] = min;
-       
+
     }
-// target Sum
-    public static int targetsum(int[] arr, int tar, int n, int[][] dp){
-        if(tar == 0){
-            return dp[n][tar] = tar == 0? 1 :0;
+
+    // target Sum
+    public static int targetsum(int[] arr, int tar, int n, int[][] dp) {
+        if (tar == 0) {
+            return dp[n][tar] = tar == 0 ? 1 : 0;
         }
-        if(dp[n][tar] != -1) return dp[n][tar];
+        if (dp[n][tar] != -1)
+            return dp[n][tar];
         int count = 0;
-        if(tar - arr[n-1] >= 0){
-            count += targetsum(arr, tar - arr[n-1], n-1, dp);
+        if (tar - arr[n - 1] >= 0) {
+            count += targetsum(arr, tar - arr[n - 1], n - 1, dp);
         }
-        count +=targetsum(arr, tar, n-1, dp);
+        count += targetsum(arr, tar, n - 1, dp);
         return dp[n][tar] = count;
     }
 
-    public static int targetsumDp(int[] arr, int Tar, int N, int[][] dp){
-        for(int n =0 ; n<=N; n++){
-            for(int tar = 0 ; tar<= Tar; tar++){
-                if(tar == 0 || n==0){
-                    dp[n][tar] = tar==0?1 : 0;
+    public static int targetsumDp(int[] arr, int Tar, int N, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int tar = 0; tar <= Tar; tar++) {
+                if (tar == 0 || n == 0) {
+                    dp[n][tar] = tar == 0 ? 1 : 0;
                     continue;
                 }
                 int count = 0;
-                if(tar - arr[n-1]>=0){
-                    count = dp[n-1][tar - arr[n-1]];
+                if (tar - arr[n - 1] >= 0) {
+                    count = dp[n - 1][tar - arr[n - 1]];
                 }
-                count += dp[n-1][tar];
+                count += dp[n - 1][tar];
                 dp[n][tar] = count;
             }
         }
         return dp[N][Tar];
     }
 
-    //01 knapsack gfg
-    static int knapSack(int W, int wt[], int val[], int n) 
-    { 
-         // your code here 
-         int[][] dp = new int[n+1][W+1];
-         for(int[] a: dp){
-             Arrays.fill(a, -1);
-         }
-         return helper(W, wt, val,n, dp);
-    } 
-    static int helper(int w, int[] wt, int[] v, int idx, int[][] dp){
-        if(w == 0 || idx == 0) return dp[idx][w] =  0 ;
-        if(dp[idx][w] != -1) return dp[idx][w];
-        
-        int max = 0;
-        if(w - wt[idx - 1] >= 0){
-            max = Math.max(max, helper(w- wt[idx - 1], wt, v, idx - 1, dp)+ v[idx -1]);
-        }
-        max = Math.max(max, helper(w, wt, v, idx - 1, dp));
-        return dp[idx][w] = max; 
-    }
-
-    //knapsack with duplicate items gfg
-    static int knapSackD(int N, int W, int val[], int wt[])
-    {
-        // code here
-        int[][] dp = new int[N+1][W+1];
-         for(int[] a: dp){
-             Arrays.fill(a, -1);
-         }
-         return helperD(W, wt, val,N, dp);
-    }
-    
-    static int helperD(int w, int[] wt, int[] v, int idx, int[][] dp){
-        if(w == 0 || idx == 0) return dp[idx][w] =  0 ;
-        if(dp[idx][w] != -1) return dp[idx][w];
-        
-        int max = 0;
-        if(w - wt[idx - 1] >= 0){
-            max = Math.max(max, helperD(w- wt[idx - 1], wt, v, idx , dp)+ v[idx -1]);
-        }
-        max = Math.max(max, helperD(w, wt, v, idx - 1, dp));
-        return dp[idx][w] = max; 
-    }
-
-    //416 Partition Equal Subset Sum
-    public int noofpart(int[] arr, int n, int tar, int[][] dp){
-        if(tar == 0 || n==0) return dp[n][tar] = tar==0? 1: 0;
-        if(dp[n][tar] != -1) return dp[n][tar];
-        
-        boolean res = false;
-        if(tar - arr[n-1] >= 0){
-            res =res || noofpart(arr, n-1, tar - arr[n-1], dp) == 1;
-        }
-        res = res || noofpart(arr, n-1, tar, dp) == 1;
-        return dp[n][tar] = res ? 1: 0;
-    }
-    public boolean canPartition(int[] nums) {
-        int sum =0;
-        for(int ele : nums) sum += ele;
-        if(nums.length == 0 || sum%2!=0) return false;
-        int n = nums.length;
-        int[][] dp = new int[n+1][sum/2 + 1];
-        for(int[] a: dp){
+    // 01 knapsack gfg
+    static int knapSack(int W, int wt[], int val[], int n) {
+        // your code here
+        int[][] dp = new int[n + 1][W + 1];
+        for (int[] a : dp) {
             Arrays.fill(a, -1);
         }
-        return noofpart(nums, n, sum/2, dp) == 1;
+        return helper(W, wt, val, n, dp);
     }
 
-    //494. Target Sum
-    public int findallways(int[] arr, int n, int tar, int sum, int[][] dp){
-        if(n == 0){
-            return dp[n][sum] = tar == sum ? 1: 0;
+    static int helper(int w, int[] wt, int[] v, int idx, int[][] dp) {
+        if (w == 0 || idx == 0)
+            return dp[idx][w] = 0;
+        if (dp[idx][w] != -1)
+            return dp[idx][w];
+
+        int max = 0;
+        if (w - wt[idx - 1] >= 0) {
+            max = Math.max(max, helper(w - wt[idx - 1], wt, v, idx - 1, dp) + v[idx - 1]);
         }
-        
-        if(dp[n][sum ] != -1) return dp[n][sum];
-        int count = findallways(arr, n-1, tar, sum - arr[n-1], dp);
-        count += findallways(arr, n-1, tar, sum + arr[n-1], dp);
-        
+        max = Math.max(max, helper(w, wt, v, idx - 1, dp));
+        return dp[idx][w] = max;
+    }
+
+    // knapsack with duplicate items gfg
+    static int knapSackD(int N, int W, int val[], int wt[]) {
+        // code here
+        int[][] dp = new int[N + 1][W + 1];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        return helperD(W, wt, val, N, dp);
+    }
+
+    static int helperD(int w, int[] wt, int[] v, int idx, int[][] dp) {
+        if (w == 0 || idx == 0)
+            return dp[idx][w] = 0;
+        if (dp[idx][w] != -1)
+            return dp[idx][w];
+
+        int max = 0;
+        if (w - wt[idx - 1] >= 0) {
+            max = Math.max(max, helperD(w - wt[idx - 1], wt, v, idx, dp) + v[idx - 1]);
+        }
+        max = Math.max(max, helperD(w, wt, v, idx - 1, dp));
+        return dp[idx][w] = max;
+    }
+
+    // 416 Partition Equal Subset Sum
+    public int noofpart(int[] arr, int n, int tar, int[][] dp) {
+        if (tar == 0 || n == 0)
+            return dp[n][tar] = tar == 0 ? 1 : 0;
+        if (dp[n][tar] != -1)
+            return dp[n][tar];
+
+        boolean res = false;
+        if (tar - arr[n - 1] >= 0) {
+            res = res || noofpart(arr, n - 1, tar - arr[n - 1], dp) == 1;
+        }
+        res = res || noofpart(arr, n - 1, tar, dp) == 1;
+        return dp[n][tar] = res ? 1 : 0;
+    }
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int ele : nums)
+            sum += ele;
+        if (nums.length == 0 || sum % 2 != 0)
+            return false;
+        int n = nums.length;
+        int[][] dp = new int[n + 1][sum / 2 + 1];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        return noofpart(nums, n, sum / 2, dp) == 1;
+    }
+
+    // 494. Target Sum
+    public int findallways(int[] arr, int n, int tar, int sum, int[][] dp) {
+        if (n == 0) {
+            return dp[n][sum] = tar == sum ? 1 : 0;
+        }
+
+        if (dp[n][sum] != -1)
+            return dp[n][sum];
+        int count = findallways(arr, n - 1, tar, sum - arr[n - 1], dp);
+        count += findallways(arr, n - 1, tar, sum + arr[n - 1], dp);
+
         return dp[n][sum] = count;
     }
-    
+
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        if(n==0) return 0;
+        if (n == 0)
+            return 0;
         int sum = 0;
-        for(int ele : nums) sum += ele;
-        if(sum < target || target < -sum) return 0;
-        int[][] dp = new int[n+1][2*sum + 1];
-        for(int[] a: dp) Arrays.fill(a, -1);
+        for (int ele : nums)
+            sum += ele;
+        if (sum < target || target < -sum)
+            return 0;
+        int[][] dp = new int[n + 1][2 * sum + 1];
+        for (int[] a : dp)
+            Arrays.fill(a, -1);
         return findallways(nums, n, target + sum, sum, dp);
     }
 
-    public static void main(String[] args){
-        int[] arr= {2,3,5,7};
-        int tar = 10;
-        int[][] dp = new int[arr.length][tar+1];
-        for(int[] a: dp)
-        Arrays.fill(a,-1);
-        int ans = combination_memo(arr, tar, 0,dp);
-        System.out.println(ans);
-        display2D(dp);
+    //Find substring pattern in given string 
+    public static int isSSmatch(String a, String b, int n, int m , int[][] dp){
+        if(n==0 || m==0){
+            if(m==0) return dp[n][m] = 1;
+            return dp[n][m] = 0;
+        }
+        if(dp[n][m] != -1) return dp[n][m];
+        char ch1 = a.charAt(n-1);
+        char ch2 = b.charAt(m-1);
+        if(ch1 == ch2 || ch2 == '*'){
+            boolean res= false;
+            res = res || isSSmatch(a, b, n-1, m-1, dp) == 1;
+            res = res || isSSmatch(a, b, n-1, m, dp) == 1;
+
+            return dp[n][m] = res ? 1: 0;
+
+        }else{
+            return dp[n][m] = isSSmatch(a, b, n-1, b.length(), dp);
+        }
+    }
+
+    public static void subStringMatch(){
+        String a= "abcde";
+        String b = "*bce";
+        int n = a.length();
+        int m = b.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        boolean res = isSSmatch(a,b,n,m,dp) == 1;
+        System.out.println(res);
+         
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = { 2, 3, 5, 7 };
+        // int tar = 10;
+        // int[][] dp = new int[arr.length][tar + 1];
+        // for (int[] a : dp)
+        //     Arrays.fill(a, -1);
+        // int ans = combination_memo(arr, tar, 0, dp);
+        // System.out.println(ans);
+        subStringMatch();
+        // display2D(dp);
     }
 }
